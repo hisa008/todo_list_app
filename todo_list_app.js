e4e9cfe8bf39874e99260f4e947f6d2667ef2bb8
hisa <hisayuki.k8@gmail.com>
2020-11-01 00:44:56 +0900

    [updata]

--- todo_list_app.js
+++ todo_list_app.js
@@ -1,5 +1,8 @@
 var btn = document.getElementById('task_btn');
-window.alert(btn);
 btn.addEventListener('click', function () {
     window.alert('クリック！');
 }, false);
+var list = document.getElementsByTagName('a');
+for (var i = 0, len = list.length; i < len; i++) {
+    window.alert(list.item(i));
+}
--- todo_list_app.ts
+++ todo_list_app.ts
@@ -3,3 +3,8 @@ const btn = document.getElementById('task_btn');
 btn.addEventListener('click', function(){
     window.alert('クリック！');
 }, false);
+
+var list = document.getElementsByTagName('a');
+for (var i = 0, len = list.length; i < len; i++) {
+    window.alert(list.item(i));
+}
