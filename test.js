16794c46c508dd5884fc50df6e77cf7e7d8b583a
hisa <hisayuki.k8@gmail.com>
2020-11-05 23:12:07 +0900

    [updata]

--- test.js
+++ test.js
@@ -35,9 +35,25 @@
 // }, false)
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('result_text').textContent =
+//         '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+//     document.getElementById('result_html').innerHTML =
+//         '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+// }, false)
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var list = document.getElementById('list');
+//     console.log('innner : ' + list.innerHTML);
+//     console.log('content : ' + list.textContent);
+// }, false)
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('result_text').textContent =
-        '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
-    document.getElementById('result_html').innerHTML =
-        '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+    document.getElementById('btn').addEventListener('click', function() {
+        var name = document.getElementById('name');
+        var result = document.getElementById('result');
+        result.innerHTML = 'こんにちは' + name.value + 'さん!';
+    }, false)
 }, false)
