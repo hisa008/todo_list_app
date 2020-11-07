eb7335d10183714dcd214fda97fc33ea9596f786
hisa <hisayuki.k8@gmail.com>
2020-11-08 00:38:34 +0900

    [updata]

--- test.js
+++ test.js
@@ -229,25 +229,56 @@
 // }, false);
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     let books = [
+//         {title: '独習PHP 第3版', price: 3200},
+//         {title: 'Javaポケットリファレンス', price: 2680},
+//         {title: 'アプリを作ろう！Android入門', price: 2000}
+//     ];
+
+//     let list = document.getElementById('list');
+
+//     let frag = document.createDocumentFragment();
+
+//     for(let i = 0, len = books.length; i < len; i++) {
+//         let b = books[i];
+//         let li = document.createElement('li');
+//         let text = document.createTextNode(b.title + ':' + b.price + '円');
+//         li.appendChild(text);
+//         frag.appendChild(li); //1
+//     }
+//     document.getElementById('list').appendChild(frag);
+// }, false);
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    let books = [
-        {title: '独習PHP 第3版', price: 3200},
-        {title: 'Javaポケットリファレンス', price: 2680},
-        {title: 'アプリを作ろう！Android入門', price: 2000}
-    ];
-
     let list = document.getElementById('list');
+    let pic = document.getElementById('pic');
+    let del = document.getElementById('del');
 
-    let frag = document.createDocumentFragment();
+    list.addEventListener('click', function(e) {
+        let isbn = e.target.getAttribute('data-isbn');
 
-    for(let i = 0, len = books.length; i < len; i++) {
-        let b = books[i];
-        let li = document.createElement('li');
-        let text = document.createTextNode(b.title + ':' + b.price + '円');
-        li.appendChild(text);
-        frag.appendChild(li); //1
-    }
-    document.getElementById('list').appendChild(frag);
+        if(isbn) {
+            let img = document.createElement('img');
+            img.src = 'http://www.wings.msn.to/books/' + isbn + '/' + isbn + '.jpg';
+            img.alt = e.target.textContent;
+            img.height = 150;
+            img.width = 108;
+
+            if(pic.getElementsByTagName('img').length > 0) {
+                pic.replaceChild(img, pic.lastChild);
+            } else {
+                del.disabled = false;
+                pic.appendChild(img);
+            }
+        }
+    }, false);
+
+    del.addEventListener('click', function() {
+        pic.removeChild(pic.lastChild);
+        del.disabled = true;
+    }, false);
 }, false);
-
 
