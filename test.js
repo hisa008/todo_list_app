8da0e12c2b177d40af5f9e6ccd2038e784ef964a
hisa <hisayuki.k8@gmail.com>
2020-11-11 23:46:43 +0900

    [start]chapter7

--- test.js
+++ test.js
@@ -415,17 +415,42 @@
 // }, false);
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('inner').addEventListener('click', function(e){
+//         window.alert('#innerリスナーが発生しました。');
+//         e.stopPropagation();
+//     }, true);
+
+//     document.getElementById('inner').addEventListener('click', function(e) {
+//         window.alert('#innerリスナー2が発生しました。');
+//     }, true);
+
+//     document.getElementById('inner').addEventListener('click', function(e) {
+//         window.alert('#outerリスナーが発生しました。');
+//     }, true);
+// }, false);
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('fm').addEventListener('submit', function(e) {
+//         if(!window.confirm('ページを送信しても良いですか？')) {
+//             // e.preventDefault();
+//             window.alert('Hi!');
+//         } else {
+//             window.alert('No!');
+//         }
+//     })
+// })
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('inner').addEventListener('click', function(e){
-        window.alert('#innerリスナーが発生しました。');
-        e.stopPropagation();
-    }, true);
-
-    document.getElementById('inner').addEventListener('click', function(e) {
-        window.alert('#innerリスナー2が発生しました。');
-    }, true);
-
-    document.getElementById('inner').addEventListener('click', function(e) {
-        window.alert('#outerリスナーが発生しました。');
-    }, true);
-}, false);
+    let timer = window.setInterval(
+        function() {
+            let dat = new Date();
+            document.getElementById('result').textContent = dat.toLocaleTimeString();
+        }, 5000);
+    document.getElementById('btn').addEventListener('click', function() {
+        window.clearInterval(timer);
+    })
+})
